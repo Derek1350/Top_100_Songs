95b42e909207d5cd18af9386f42c689cdfe504b6
Chittanshu Singh <119718508+Derek1350@users.noreply.github.com>
2023-09-01 02:49:23 +0530

    Update authorize.py

--- authorize.py
+++ authorize.py
@@ -1,8 +1,8 @@
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-CLIENT_ID = "c1d08315a1d6428ca10982e127609784"
-CLIENT_SECRET = "a95d73741d93442cbab0cf8a6068e0ca"
+CLIENT_ID = "Your Client ID"
+CLIENT_SECRET = "Your Secret ID"
 REDIRECT_URI="https://example.com/"
 USERNAME="2n3vohg923lg7ustb6fang577"
 scope="playlist-modify-private"
